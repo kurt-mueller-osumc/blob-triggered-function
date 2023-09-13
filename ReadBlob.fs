8a631115c69f0d03caf410dff14fcc18a2dca1d8
Kurt Mueller <kurt.mueller@osumc.edu>
2023-09-13 11:06:03 -0400

    Initialize blob triggered function

---
+++ ReadBlob.fs
@@ -0,0 +1,29 @@
+namespace Comrit.Functions
+
+open System
+open System.IO
+open Microsoft.Azure.Functions.Worker
+open Microsoft.Extensions.Logging
+
+module ReadBlob =
+
+    [<Function("ReadBlob")>]
+    let run
+        (
+            [<BlobTrigger("samples-workitems/{name}", Connection = "")>] myBlob: Stream,
+            name: string,
+            context: FunctionContext
+        ) =
+        let logger
+            = context.GetLogger "ReadBlob"
+
+        use blobStreamReader
+            = new StreamReader(myBlob)
+
+        let blobContent
+            = blobStreamReader.ReadToEndAsync() |> Async.AwaitTask
+
+        let msg =
+            sprintf "F# Blob trigger function Processed blob\nName: %s \n Data: %s" name blobContent
+
+        logger.LogInformation msg
