4826575ffc172a2fbfc054870264f94712c298ff
Kurt Mueller <kurtronaldmueller@gmail.com>
2023-09-14 21:57:00 -0400

    Configure blob storage extension

--- Program.fs
+++ Program.fs
@@ -3,18 +3,14 @@ open Microsoft.Azure.Functions.Worker
 
 [<EntryPoint>]
 let main args =
-    let host =
-        HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
-            .Build()
+    let hostBuilder = new HostBuilder()
 
-        // If using the Cosmos, Blob or Tables extension, you will need configure the extensions manually using the extension methods below.
-        // Learn more about this here: https://go.microsoft.com/fwlink/?linkid=2245587
-        // ConfigureFunctionsWorkerDefaults(fun (context: HostBuilderContext) (appBuilder: IFunctionsWorkerApplicationBuilder) ->
-        //     appBuilder.ConfigureCosmosDBExtension() |> ignore
-        //     appBuilder.ConfigureBlobStorageExtension() |> ignore
-        //     appBuilder.ConfigureTablesExtension() |> ignore
-        // ) |> ignore
+    hostBuilder.ConfigureFunctionsWorkerDefaults(fun (context: HostBuilderContext) (appBuilder: IFunctionsWorkerApplicationBuilder) ->
+        appBuilder.ConfigureBlobStorageExtension() |> ignore
+    ) |> ignore
+
+    let host = hostBuilder.Build()
 
     host.Run()
+
     0
